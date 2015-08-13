ba51f6ca71b9cb9ffcea763b3c5cda2ef0e58163
duongnhu <d.binhnhu@gmail.com>
2015-08-14 00:27:38 +1000

    setup service

--- src/CodeFury/CaptchaServiceProvider.php
+++ src/CodeFury/CaptchaServiceProvider.php
@@ -6,6 +6,11 @@
  * Time: 2:22 PM
  */
 
+use
+
 class CaptchaServiceProvider extends \App\Providers\AppServiceProvider {
 
+    public function boot() {
+        Route::get('captcha', "\CodeFury\CaptchaController@get_captcha_url");
+    }
 }
